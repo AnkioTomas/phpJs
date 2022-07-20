513642faee7af000a444946bba79591508ea3a85
ankio <dream@dreamn.cn>
2022-07-20 16:36:49 +0800

    fix: bin2hex.js && hex2bin.js

--- strings/hex2bin.js
+++ strings/hex2bin.js
@@ -1,8 +1,8 @@
 function hex2bin(hex) {
-    var bytes = [];
-
-    for (var i = 0; i < hex.length - 1; i += 2)
-        bytes.push(parseInt(hex.substr(i, 2), 16));
-
-    return String.fromCharCode.apply(String, bytes);
+    var length = hex.length / 2;
+    var result = new Uint8Array(length);
+    for (var i = 0; i < length; ++i) {
+        result[i] = parseInt(hex.slice(i * 2, i * 2 + 2), 16);
+    }
+    return result;
 }
